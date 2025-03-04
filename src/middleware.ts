3ea5f081e062cd0441f6d491c5e64873498c8dc1
Rajat1120 <rajat812678@gmail.com>
2025-03-05 01:34:56 +0530

    create zap backend

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,31 @@
 import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { JWT_PASSWORD } from "./config";
 
-export function authMiddleware(
+export const authMiddleware = (
   req: Request,
   res: Response,
   next: NextFunction
-) {
-  next();
-}
+): void => {
+  const token = req.headers.authorization;
+
+  try {
+    if (!token) {
+      res.status(401).json({
+        message: "No token provided",
+      });
+      return;
+    }
+
+    const payload = jwt.verify(token, JWT_PASSWORD);
+
+    // Type assertion to add id to the request
+    (req as any).id = (payload as any).id;
+
+    next();
+  } catch (e) {
+    res.status(403).json({
+      message: "You are not logged in",
+    });
+  }
+};
--- src/router/user.ts
+++ src/router/user.ts
@@ -1,10 +1,93 @@
+import { JWT_PASSWORD } from "./../config";
 import { Router } from "express";
+import { SigninSchema, SignupSchema } from "../types/type";
+import { prismaClient } from "../db/database";
+import jwt from "jsonwebtoken";
+import { authMiddleware } from "../middleware";
 
 const router = Router();
 
-router.post("/signup", (req, res) => {});
+router.post("/signup", async (req, res): Promise<any> => {
+  const body = req.body;
+  const parsedData = SignupSchema.safeParse(body);
 
-router.post("/signin", (req, res) => {});
-router.get("/user", (req, res) => {});
+  if (!parsedData.success) {
+    return res.status(411).json({ message: "Incorrect inputs" });
+  }
+
+  const userExist = await prismaClient.user.findFirst({
+    where: { email: parsedData.data.username },
+  });
+
+  if (userExist) {
+    return res.status(403).json({ message: "User already exists" });
+  }
+
+  await prismaClient.user.create({
+    data: {
+      email: parsedData.data.username,
+      // TODO: hash password
+      password: parsedData.data.password,
+      name: parsedData.data.name,
+    },
+  });
+
+  // await send email verification
+
+  return res.json({
+    message: "please verify your account by checking your email",
+  });
+});
+
+router.post("/signin", async (req, res): Promise<any> => {
+  const body = req.body;
+  const parsedData = SigninSchema.safeParse(body);
+
+  if (!parsedData.success) {
+    return res.status(411).json({ message: "Incorrect inputs" });
+  }
+
+  const user = await prismaClient.user.findFirst({
+    where: {
+      email: parsedData.data.username,
+      password: parsedData.data.password,
+    },
+  });
+
+  if (!user) {
+    return res.status(403).json({ message: "User not found" });
+  }
+
+  // sign the JWT
+  const token = jwt.sign(
+    {
+      id: user.id,
+    },
+    JWT_PASSWORD
+  );
+
+  res.json({
+    token: token,
+  });
+});
+
+router.get("/", authMiddleware, async (req, res): Promise<any> => {
+  // TODO: Fix the type
+  // @ts-ignore
+  const id = req.id;
+  const user = await prismaClient.user.findFirst({
+    where: {
+      id,
+    },
+    select: {
+      name: true,
+      email: true,
+    },
+  });
+
+  return res.json({
+    user,
+  });
+});
 
 export const userRouter = router;
